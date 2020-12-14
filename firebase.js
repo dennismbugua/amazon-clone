0190632c3364357c3932bb6741c8625fb8a20cc2
Dennis Mbugua <dennis@starlaunch.org>
2020-12-15 00:06:55 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,6 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-/*  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
-  authDomain: "challenge-4b2b2.firebaseapp.com",
-  databaseURL: "",
-  projectId: "challenge-4b2b2",
-  storageBucket: "challenge-4b2b2.appspot.com",
-  messagingSenderId: "962418448875",
-  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",*/
-
-/*    apiKey: "AIzaSyAGwTGLQ7dnJd-1wMd35rRzPhDxWpxqgPo",
-    authDomain: "clone-c2033.firebaseapp.com",
-    projectId: "clone-c2033",
-    storageBucket: "clone-c2033.appspot.com",
-    messagingSenderId: "7276015817",
-    appId: "1:7276015817:web:177a6898500379c1ac9d18"*/
 
     apiKey: "AIzaSyDX0xSalwDs5hc9I6Y9BRC4LzX4WM75Dq4",
     authDomain: "clone-3221b.firebaseapp.com",
