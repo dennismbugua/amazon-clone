0c81f02c6b8a6e3cca782b0058bca1f7ac4d7990
Dennis Mbugua <dennis@starlaunch.org>
2020-12-14 23:58:42 +0300

    Add files via upload

---
+++ build/precache-manifest.3cb4831bff836dbd397ca90d35cdf3fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9536a8a4ebf4ca76c21d99dd24f89954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44672242292b4fd00cfc",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "0c688d216e648887c618",
+    "url": "/static/js/2.77aae392.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/static/js/2.77aae392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44672242292b4fd00cfc",
+    "url": "/static/js/main.a0ae65cf.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
