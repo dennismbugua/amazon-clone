e492e73a27d2f8c50e3f0cb2c903ea9d42c53d50
Dennis Mbugua <dennis@starlaunch.org>
2020-12-15 00:56:24 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,19 +1,28 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
+/*  apiKey: "AIzaSyCw-li0IscS2rJr68dSjoJL3KM3eFq5PfE",
+  authDomain: "twitter-clone-1faac.firebaseapp.com",
+  databaseURL: "",
+  projectId: "twitter-clone-1faac",
+  storageBucket: "twitter-clone-1faac.appspot.com",
+  messagingSenderId: "316208638057",
+  appId: "1:316208638057:web:75a37bf24fab32ff145af8",
+  measurementId: "G-HGFP2LDXP9",*/
 
-    apiKey: "AIzaSyDX0xSalwDs5hc9I6Y9BRC4LzX4WM75Dq4",
-    authDomain: "clone-3221b.firebaseapp.com",
-    databaseURL: "https://clone-3221b.firebaseio.com",
-    projectId: "clone-3221b",
-    storageBucket: "clone-3221b.appspot.com",
-    messagingSenderId: "306032674437",
-    appId: "1:306032674437:web:e3ff75d232b9edc558cbec"
+    apiKey: "AIzaSyCk9H7hZxGP1ehDRIULM1GfSkTLQiAcxdw",
+    authDomain: "twitter-clone-bddb7.firebaseapp.com",
+    databaseURL: "https://twitter-clone-bddb7.firebaseio.com",
+    projectId: "twitter-clone-bddb7",
+    storageBucket: "twitter-clone-bddb7.appspot.com",
+    messagingSenderId: "135667678724",
+    appId: "1:135667678724:web:4bb65f023f11112eb93f51",
+
+
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
-const auth = firebase.auth();
 
-export { db, auth };
+export default db;
--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import reducer, { initialState } from "./reducer";
-import { StateProvider } from "./StateProvider";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
